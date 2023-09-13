442d3f7e617eb9137ff63f8b8555ecd234835a49
Ade3164 <Adebayoidris051@gmail.com>
2023-09-13 15:39:51 +0100

    HNG

--- api/app.py
+++ api/app.py
@@ -1,11 +1,61 @@
+import os
 from flask import Flask, jsonify, request
 import mysql.connector
-from config import DATABASE_CONFIG  
+from dotenv import load_dotenv
 
+# Load environmental variables from .env
+load_dotenv()
+
+# Load database configuration from environmental variables
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+
+# Initialize the Flask application
 app = Flask(__name__)
 
-db = mysql.connector.connect(**DATABASE_CONFIG)
+# Create the database connection
+db = mysql.connector.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_NAME
+)
+
 cursor = db.cursor()
+
+# Function to create the "persons" table if it doesn't exist
+def create_persons_table():
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS persons (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+    )
+    """
+    cursor.execute(create_table_query)
+    db.commit()
+
+# Call the function to create the table
+create_persons_table()
+
+# Function to insert initial data into the "persons" table
+def insert_initial_data():
+    insert_data_query = """
+    INSERT INTO persons (name) VALUES (%s)
+    """
+    data = [("Alice",), ("Bob",), ("Charlie",)]
+    cursor.executemany(insert_data_query, data)
+    db.commit()
+
+# Call the function to insert initial data
+insert_initial_data()
+
+@app.route('/', methods=['GET'])
+def home():
+  return "Welcome HNG CRUD operation /api"
 
 @app.route("/api/persons", methods=["GET"])
 def get_persons():
@@ -14,17 +64,19 @@ def get_persons():
     persons_list = [{"id": id, "name": name} for id, name in persons_data]
     return jsonify(persons_list)
 
-@app.route("/api/persons/<person_identifier>", methods=["GET"])
-def get_person(person_identifier):
-    # Check if person_identifier is numeric (ID) or alphanumeric (Name)
-    if person_identifier.isdigit():
-        query = "SELECT id, name FROM persons WHERE id = %s"
-    else:
-        query = "SELECT id, name FROM persons WHERE name = %s"
-    
-    cursor.execute(query, (person_identifier,))
+@app.route("/api/persons/<int:id>", methods=["GET"])
+def get_person(id):
+    cursor.execute("SELECT id, name FROM persons WHERE id = %s", (id,))
     person_data = cursor.fetchone()
-    
+    if person_data:
+        return jsonify({"id": person_data[0], "name": person_data[1]})
+    else:
+        return jsonify({"error": "Person not found"}), 404
+
+@app.route("/api/persons/<string:name>", methods=["GET"])
+def get_person_by_name(name):
+    cursor.execute("SELECT id, name FROM persons WHERE name = %s", (name,))
+    person_data = cursor.fetchone()
     if person_data:
         return jsonify({"id": person_data[0], "name": person_data[1]})
     else:
--- api/config.py
+++
@@ -1,8 +0,0 @@
-# Database Configuration
-DATABASE_CONFIG = {
-    'user': '***',
-    'password': '***.',
-    'host': 'localhost',  
-    'database': 'Database name',
-    'port': 3306,  
-}
